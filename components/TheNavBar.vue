e8a06b6b2e921ed448e7b8cb0644a0d4b0914fa1
Joshua Galit <joshuaimalay@gmail.com>
2021-01-15 17:01:23 +0800

    add logo and menu icon

---
+++ components/TheNavBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="px-6 py-3 bg-gray-900">
+    <div class="flex items-center justify-between">
+      <div class="flex-shrink-0">
+        <img class="h-5" src="https://nuxtjs.org/logos/nuxtjs-typo.svg" alt="logo">
+      </div>
+      <nav>
+        <button class="block focus:outline-none rounded focus:ring-2 ring-teal-600">
+          <svg class="w-8 h-8 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#000000"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path></svg>
+        </button>
+      </nav>
+    </div>
+  </header>
+</template>
