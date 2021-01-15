bb363ba2905e3008d6dea2159e605857b9a6e19d
Joshua Galit <joshuaimalay@gmail.com>
2021-01-16 06:56:06 +0800

    add xs of 480 pixel  breakpoint in screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,14 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screen: {
+      'xs': '480px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       fontFamily: {
         primary: 'Poppins, sans-serif',
