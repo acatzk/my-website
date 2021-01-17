ad98cc94cc8abbe5f33633ee6933d11b64eb447a
Joshua Galit <joshuaimalay@gmail.com>
2021-01-17 23:43:46 +0800

    add second section for personal info style

--- pages/index.vue
+++ pages/index.vue
@@ -4,15 +4,15 @@
       <div class="container mx-auto max-w-6xl">
         <div class="py-9 px-6 flex flex-col md:flex-row-reverse items-center md:justify-around">
           <div class="flex-shrink-0 py-3 sm:py-6">
-            <img class="w-56 sm:w-auto rounded-full shadow-xl border ring-4 ring-white transform transition hover:-rotate-6 ease-in-out duration-500 object-cover" src="https://avatars3.githubusercontent.com/u/38458781?s=460&u=8fc4b0725bdcdd4624fc99ac4fb4ac64ee8768cc&v=4" alt="image">
+            <img class="h-72 w-72 rounded-full shadow-xl border ring-4 ring-white transform transition hover:-rotate-6 ease-in-out duration-500 object-cover" src="@/assets/images/avatar.png" alt="image">
           </div>
           <div class="max-w-lg md:max-w-xl py-6">
-            <h1 class="uppercase font-semibold tracking-tight text-white text-3xl  md:text-5xl lg:text-6xl">
+            <h1 class="uppercase font-semibold text-center sm:text-left tracking-tight text-white text-3xl  md:text-5xl lg:text-6xl">
               Joshua <span class="font-normal tracking-wide bg-gradient-to-b from-cyan-100 to-blue-100 bg-clip-text text-transparent">Galit</span>
             </h1>
-            <p class="pt-6 text-sm sm:text-base text-white leading-loose">I am 21-year old dedicated and enthusiastic web developer trying to make the world look a bit better. I loved rebuilding existing social websites and addict in single page app and playin' guitar.</p>
+            <p class="pt-6 text-sm sm:text-base text-white leading-relaxed">I am 21-year old dedicated and enthusiastic web developer trying to make the world look a bit better. I loved rebuilding existing social websites and addict in single page app and playin' guitar.</p>
             <div class="space-x-0 xs:space-x-2">
-              <a href="#" class="inline-block bg-white mt-5 px-8 py-2 text-blue-500 rounded-full font-bold uppercase text-sm tracking-wide transform transition hover:scale-105 ease-in-out duration-300">
+              <a href="#" class="inline-block bg-white mt-5 px-9 py-2 text-blue-400 rounded-full font-bold uppercase text-sm tracking-wide transform transition hover:scale-105 ease-in-out duration-300">
                 Read more
               </a>
               <a href="#" class="inline-block border-2 border-white mt-5 px-8 py-2 text-white rounded-full font-bold uppercase text-sm tracking-wide transform transition hover:scale-105 ease-in-out duration-300">
@@ -23,66 +23,34 @@
         </div>
       </div>
     </section>
-    <section class="bg-gray-100">
-      <div class="container mx-auto max-w-6xl min-h-screen">
-        <div class="py-16 text-center">
-          <h2 class="bg-lightBlue-100 inline-block px-6 py-2 font-primary font-bold uppercase text-sm rounded-full">Features</h2>
-          <h3 class="mt-3 bg-gradient-to-b from-cyan-300 to-blue-500 bg-clip-text text-transparent text-xl font-bold">Amazing Features</h3>
-        </div>
-        <div class="lg:grid lg:grid-cols-3 lg:gap-5 px-6 flex flex-col items-center">
-
-          <div class="space-y-6">
-            <div class="flex justify-between space-x-4 mr-4">
-              <div class="max-w-lg">
-                <h4 class="text-2xl font-bold">User Friendly</h4>
-                <p class="pt-3 text-gray-500 leading-loose text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor labore a at asperiores, natus alias.</p>
+    <section class="bg-white">
+      <div class="container mx-auto max-w-6xl">
+        <div class="grid grid-cols-1 lg:grid-cols-8 gap-14 py-9 px-6">
+          <div class="lg:col-span-5">
+            <div class="overflow-hidden border border-gray-200 border-opacity-75 rounded-md shadow">
+              <div class="flex items-center justify-between w-full p-3 bg-gray-100">
+                <div class="flex items-center space-x-2">
+                  <div class="w-3 h-3 bg-red-300 border border-red-400 rounded-full"></div>
+                  <div class="w-3 h-3 bg-yellow-300 border border-yellow-400 rounded-full"></div>
+                  <div class="w-3 h-3 bg-green-300 border border-green-400 rounded-full"></div>
+                </div>
               </div>
-              <div class="bg-gradient-to-r from-blue-500 to-cyan-300 h-16 w-16 rounded-full flex-shrink-0"></div>
+              <pre class="w-full p-5 overflow-auto font-mono leading-5 text-gray-700 break-all whitespace-pre-wrap bg-white select-text">
+                <div class="flex items-baseline space-x-4 leading-relaxed" v-for="(w, i) in whoami" :key="i">
+                  <span class="text-sm leading-relaxed text-gray-400 select-none">{{ w.id }}</span>
+                  <span class="text-gray-400">{{ w.title }}</span>
+                  <span class="text-gray-700">{{ w.text }}</span>
+                </div>
+              </pre>
             </div>
-            <div class="flex justify-between  space-x-4 mr-4">
-              <div class="max-w-lg">
-                <h4 class="text-2xl font-bold">Super Speed</h4>
-                <p class="pt-3 text-gray-500 leading-loose text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor labore a at asperiores, natus alias.</p>
+            </div>
+            <div class="items-center justify-center hidden col-span-2 lg:flex">
+              <h3 class="text-2xl leading-normal text-blue-900 uppercase">
+                <span>Who I am</span>
+                <div class="w-12 h-1 bg-blue-900 bg-opacity-75">
               </div>
-              <div class="bg-gradient-to-r from-blue-500 to-cyan-300 h-16 w-16 rounded-full flex-shrink-0"></div>
-            </div>
-            <div class="flex justify-between  space-x-4 mr-4">
-              <div class="max-w-lg">
-                <h4 class="text-2xl font-bold">24/7 Support</h4>
-                <p class="pt-3 text-gray-500 leading-loose text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor labore a at asperiores, natus alias.</p>
-              </div>
-              <div class="bg-gradient-to-r from-blue-500 to-cyan-300 h-16 w-16 rounded-full flex-shrink-0"></div>
-            </div>
+            </h3>
           </div>
-
-          <div class="flex-shrink-0 py-8">
-            <img class="h-80" src="@/assets/svgs/snow-globe.svg" alt="image">
-          </div>
-
-          <div class="space-y-6">
-            <div class="flex justify-between space-x-4 mr-4 text-right">
-              <div class="bg-gradient-to-r from-blue-500 to-cyan-300 h-16 w-16 rounded-full flex-shrink-0"></div>
-              <div class="max-w-lg">
-                <h4 class="text-2xl font-bold">Secure</h4>
-                <p class="pt-3 text-gray-500 leading-loose text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor labore a at asperiores, natus alias.</p>
-              </div>
-            </div>
-            <div class="flex justify-between  space-x-4 mr-4 text-right">
-              <div class="bg-gradient-to-r from-blue-500 to-cyan-300 h-16 w-16 rounded-full flex-shrink-0"></div>
-              <div class="max-w-lg">
-                <h4 class="text-2xl font-bold">Awesome Rating</h4>
-                <p class="pt-3 text-gray-500 leading-loose text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor labore a at asperiores, natus alias.</p>
-              </div>
-            </div>
-            <div class="flex justify-between  space-x-4 mr-4 text-right">
-              <div class="bg-gradient-to-r from-blue-500 to-cyan-300 h-16 w-16 rounded-full flex-shrink-0"></div>
-              <div class="max-w-lg">
-                <h4 class="text-2xl font-bold">Award Winning</h4>
-                <p class="pt-3 text-gray-500 leading-loose text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor labore a at asperiores, natus alias.</p>
-              </div>>
-            </div>
-          </div>
-
         </div>
       </div>
     </section>
@@ -93,6 +61,48 @@
   export default {
     head: {
       title: 'Snow~hat'
+    },
+    data () {
+      return {
+        whoami: [
+          {
+            id: 1,
+            title: '// Personal information'
+          },
+          {
+            id: 2,
+            text: 'const name = "Joshua Galit";'
+          },
+          {
+            id: 3,
+            text: 'const location = "Bato, Leyte";'
+          },
+          {
+            id: 4,
+            text: ''
+          },
+          {
+            id: 5,
+            title: '// Hobbies'
+          },
+          {
+            id: 6,
+            text: 'const hobbies = ['
+          },
+          {
+            id: 7,
+            text: ' "Food",'
+          },
+          {
+            id: 8,
+            text: ' "Writing useless comments in code"'
+          },
+          {
+            id: 9,
+            text: '];'
+          }
+        ]
+      }
     }
   }
 </script>
