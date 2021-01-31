0126bdee8020939b69d481f671f4af00c07e1409
Joshua Galit <joshuaimalay@gmail.com>
2021-01-31 12:03:53 +0800

    add drop down menu on small screen size

--- components/TheNavBar.vue
+++ components/TheNavBar.vue
@@ -1,17 +1,13 @@
 <template>
-  <header class="bg-gray-900 sticky top-0 md:bg-gradient-to-r from-blue-500 to-cyan-300 z-10">
-    <div class="flex items-center justify-between px-6 container mx-auto max-w-6xl">
-      <router-link to="/" class="flex items-center space-x-3">
-        <div class="flex-shrink-0 py-3">
-          <img class="h-8 w-8 rounded-full object-cover border-2" src="@/assets/images/avatar.png" alt="logo">
-        </div>
-        <span class="font-semibold text-white tracking-wider text-base font-sans">
-          Joshua Galit
-        </span>
-      </router-link>
-      <nav>
-        <button class="block focus:outline-none rounded focus:ring-2 ring-teal-600 md:hidden">
-          <svg class="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#000000"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path></svg>
+  <div class="relative bg-gradient-to-r from-blue-500 to-cyan-300">
+    <div class="flex justify-between items-center container mx-auto max-w-6xl px-6">
+      <nuxt-link to="/" class="flex items-center space-x-2 pt-4 md:pt-1">
+        <img class="h-8 w-8 border-2 border-white object-cover rounded-full" src="~/assets/images/avatar.png" alt="avatar">
+        <span class="text-white">Joshua Galit</span>
+      </nuxt-link>
+      <div>
+        <button @click.prevent="toggle = !toggle" class="text-white block md:hidden">
+          <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path></svg>
         </button>
         <div class="hidden md:block">
           <ul class="flex items-center pt-4 space-x-6 lg:space-x-8">
@@ -26,31 +22,70 @@
             </li>
           </ul>
         </div>
-      </nav>
+      </div>
     </div>
-  </header>
+
+    <div class="absolute top-0 inset-x-0 p-2 z-50" v-show="toggle">
+      <div class="shadow-lg rounded-lg">
+        <div class="bg-white shadow-xs rounded-lg pt-5 pb-8 px-5 space-y-7">
+          <div class="flex justify-between items-center">
+            <nuxt-link to="/" class="flex items-center space-x-2">
+              <img class="h-8 w-8 object-cover rounded-full" src="~/assets/images/avatar.png" alt="avatar">
+              <span>Joshua Galit</span>
+            </nuxt-link>
+            <div>
+              <button class="text-gray-400 rounded focus:outline-none hover:bg-gray-50 focus:bg-gray-100 focus:text-gray-500 transition ease-in-out duration-150" @click.prevent="toggle = !toggle">
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+              </button>
+            </div>
+          </div>
+          <div>
+            <nav class="space-y-7">
+              <ul v-for="(link, i) in links" :key="i">
+                <li>
+                  <nuxt-link :to="link.to" class="flex items-center space-x-4">
+                    <div class="flex items-center justify-center h-12 w-12 rounded-md bg-gradient-to-r from-blue-500 to-cyan-300 text-white">
+                      <span v-html="link.icon"></span>
+                    </div>
+                    <span class="font-medium text-gray-900">
+                      {{ link.title }}
+                    </span>
+                  </nuxt-link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
     data () {
       return {
+        toggle: false,
         setLink: 'About',
         links: [
           {
             title: 'About',
+            icon: '<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>',
             to: '/'
           },
           {
             title: 'Portfolio',
+            icon: '<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>',
             to: '/portfolio'
           },
           {
             title: 'Blog',
+            icon: '<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"></path></svg>',
             to: '/blog'
           },
           {
             title: 'Contact',
+            icon: '<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 19v-8.93a2 2 0 01.89-1.664l7-4.666a2 2 0 012.22 0l7 4.666A2 2 0 0121 10.07V19M3 19a2 2 0 002 2h14a2 2 0 002-2M3 19l6.75-4.5M21 19l-6.75-4.5M3 10l6.75 4.5M21 10l-6.75 4.5m0 0l-1.14.76a2 2 0 01-2.22 0l-1.14-.76"></path></svg>',
             to: '/contact'
           }
         ]
